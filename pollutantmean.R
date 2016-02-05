804f342c23ca255c0ddfa1d9927f6271a7024baa
exseivier <mariom1956@gmail.com>
2016-02-04 23:16:28 -0600

    Create pollutantmean.R

---
+++ pollutantmean.R
@@ -0,0 +1,19 @@
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+        valuesOfvalues <- c()
+        for (i in id) {
+                if (i < 10){
+                        filename <- paste("00", i, sep="")
+                } else if (i >= 10 & i < 100){
+                        filename <- paste("0", i, sep="")
+                } else {
+                        filename <- paste("", i, sep="")
+                }
+                filepath <- paste(directory, filename, ".csv", sep="")
+                df <- read.csv(filepath, header=TRUE)
+                values <- df[pollutant]
+                valuesOfvalues <- c(valuesOfvalues, values[,1])
+        }
+        valuesOfvalues <- valuesOfvalues[!is.na(valuesOfvalues)]
+        mean(valuesOfvalues)
+
+}
